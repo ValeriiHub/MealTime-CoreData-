30ee49135a9fbf0b3ec7a359b9e1558eb880d3f8
ValeriiHub <p04taera@gmail.com>
2021-07-31 17:57:55 +0300

    Initial Commit

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Valerii D on 31.07.2021.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Valerii D on 31.07.2021.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
